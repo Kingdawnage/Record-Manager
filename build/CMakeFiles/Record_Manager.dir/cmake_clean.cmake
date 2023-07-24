27272f91afe981d0f7602920fade152d5bf4edc0
Katchy Ugboma <katchymemo@yahoo.com>
2023-07-24 09:27:09 +0200

    Initialized with cmake

---
+++ build/CMakeFiles/Record_Manager.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Record_Manager.dir/src/program.cpp.obj"
+  "CMakeFiles/Record_Manager.dir/src/program.cpp.obj.d"
+  "Record_Manager.exe"
+  "Record_Manager.exe.manifest"
+  "Record_Manager.pdb"
+  "libRecord_Manager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Record_Manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
